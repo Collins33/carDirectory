5f7fdcd0261fa1c10a370ebae9fa0ffa1ccb0286
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-11-23 22:57:17 +0300

    I have added the methods from the car class

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3.6
+from car import Car
 
-print ("hello")
+def createCar(model,year,color,price):
+    newCar= Car(model,year,color,price)
+    return newCar
+
+def savCar(car):
+    car.saveCar()
+
+def delCar(car):
+    car.deleteCar()
+
+def findCar(model):
+    return Car.findByModel(model)
+
+def checkExistance(model):
+    return Car.carExist(model)
+
+def showAllCars():
+    return Car.displayCars()                
